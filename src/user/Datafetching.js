11892011a68a6247bf1b5ad0a17ee14c27cf5f16
Oluwatosin Lasisi <lasisioluwatomisin@gmail.com>
2022-10-31 22:27:55 +0100

    new fix

--- src/user/Datafetching.js
+++ src/user/Datafetching.js
@@ -1,48 +1,75 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Card from "./Card.js"
+import Card from "./Card.js";
 function Datafetching() {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
-const url = "https://randomuser.me/api/?results=50"
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+  const url = "https://randomuser.me/api/?results=50";
 
-  useEffect( ()=>{
-    const fetchUsers = async()=>{ 
-    const res = await axios.get(url)
-    setData(res.data.results)
-      setIsLoading(false)
-      setTotalPages
-      (Math.ceil (res.data.results.length / USER_PER_PAGE));
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await axios.get(url);
+      setData(res.data.results);
+      setIsLoading(false);
     };
-    fetchUsers()
-  },[]);
-  const handleClick = (num) => {
-    setPage(num);
-  }
+    fetchUsers();
+  }, []);
+  //const handleClick = (num) => {
+  //setPage(num);
+  //};
 
-  if(isLoading) {
-    return (
-      <div className="Contract Employee">
-        {IsLoading ? (<p>loading...</p> ) : (
-    
-    <>
-      <Users users={users} page={page} />
-          <Pagination totalPages={totalPages} handleClick={handleClick} setPage={setPage} page={page} />
-          <ErrorBoundary />
-        </>)};
-  <div className="refreshLink">
-  <NavLink to="/">Refresh</NavLink>
-  </div>
-      </div>
+  //if (isLoading) {
+  // return
+  //<h2>loading...</h2>;
+
+  const indexOfLastCard = currentPage * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCard = Card.slice(indexOfFirstCard, indexOfLastCard);
 
   return (
-    <div style={{display: "flex", flexDirection: "column", alignItems:"center"}}> 
-      {data.map((item, index)=>{
-      return <Card key={index} name={item.name} gender={item.gender} email={item.email} />
-      })}
-
+    <div className="">
+      <h1 className="dataOfEmployee">Employee Data</h1>
+      <Card
+        key={index}
+        name={item.name}
+        gender={item.gender}
+        email={item.email}
+        isloading={loading}
+      />
     </div>
   );
 }
+//   <>
+//     <Users users={users} page={page} />
+//         <Pagination totalPages={totalPages} handleClick={handleClick} setPage={setPage} page={page} />
+//         <ErrorBoundary />
+//       </>)};
+// <div className="refreshLink">
+// <NavLink to="/">Refresh</NavLink>
+// </div>
+
+//   return (
+//     <div
+//       className="listEmployee"
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//       }}>
+//       {data.map((item, index) => {
+//         return (
+//           <Card
+//             key={index}
+//             name={item.name}
+//             gender={item.gender}
+//             email={item.email}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// }
 
 export default Datafetching;

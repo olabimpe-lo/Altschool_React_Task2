bc0929ccdeac003107fba72b58847aa577c12543
Oluwatosin Lasisi <lasisioluwatomisin@gmail.com>
2023-01-02 00:19:55 +0100

    testing branch

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="homepage">
       <h2> BIMARI </h2>
-      <h4>No 1 online male fashion store</h4>
+      <h4 className="store_header">No 1 online male fashion store</h4>
       <img
         src={require("../images/manshopping.jpg")}
         alt="A young man looking through collections of clothes to shop"

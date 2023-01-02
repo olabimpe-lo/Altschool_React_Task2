6029e5a4a5663fee486aa615b3942e6508ad0db7
Oluwatosin Lasisi <lasisioluwatomisin@gmail.com>
2023-01-02 01:28:48 +0100

    new css fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 // import "./index.css";
 
-const path = window.location.pathname;
+
 const Navbar = () => {
   return (
     <div>
       <nav className="nav">
-        {/* <Link to="/" className="company-site">
+        <Link to="/" className="company-site">
           BIMARI
-        </Link> */}
+        </Link>
         <ul>
           <li className="nav_features">
             <Link to="/">Home</Link>
@@ -19,6 +19,12 @@ const Navbar = () => {
           </li>
           <li>
             <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/does-not-exist">Error Page</Link>
+          </li>
+          <li>
+            <Link to="/page-not-found">Error Boundary</Link>
           </li>
         </ul>
       </nav>
